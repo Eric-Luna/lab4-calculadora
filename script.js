12f7ec057768c4a3135a6043024da6aeefcd0936
Eric Luna <32916033+Eric-Luna@users.noreply.github.com>
2018-08-29 12:26:29 -0300

    Subido ejercicio al repositorio Agregado historial de resultados

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+$(".btn").click(function() {
+    console.log(this.innerHTML);
+    if(this.innerHTML != '=' && this.innerHTML != 'C'){
+        let txt = $("#calculator-result").val() + this.innerHTML; 
+        $("#calculator-result").val(txt);
+    }
+    else if(this.innerHTML == '='){
+        let txt_unmodified = $("#calculator-result").val();
+        let txt = $("#calculator-result").val();
+        let resultado;
+        if(txt.includes("+")){
+            txt = txt.split("+");
+            resultado = parseFloat(txt[0]) + parseFloat(txt[1]);
+        }
+        else if(txt.includes("-")){
+            txt = txt.split("-");
+            resultado = parseFloat(txt[0]) - parseFloat(txt[1]);            
+        }
+        else if(txt.includes("*")){
+            txt = txt.split("*");
+            resultado = parseFloat(txt[0]) * parseFloat(txt[1]);            
+        }
+        else if(txt.includes("/")){
+            txt = txt.split("/");
+            resultado = parseFloat(txt[0]) / parseFloat(txt[1]);            
+        }
+        else{
+            resultado = "Error";
+        }
+
+        if(resultado == "Error"){
+            $("#listHistorial").after('<li>' + resultado +'</li>');
+        }else{
+            $("#listHistorial").after('<li>' + txt_unmodified + ' = ' + resultado + '</li>');
+        }
+        $("#calculator-result").val(resultado);
+    }
+    else{
+        $("#calculator-result").val("");
+    }
+});
+
+});
